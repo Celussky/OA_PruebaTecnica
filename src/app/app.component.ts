a59284bbc00629f26b15047699d02a4ae1950a35
Celussky <gabri.bernal@gmail.com>
2024-12-20 22:49:13 -0500

    home sin footer terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { NgOptimizedImage } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    NgOptimizedImage,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+const tituloPagina = "QuickBites"
+
+export const routes: Routes = [
+    { path: 'home', title: `Home | ${tituloPagina}`, component: HomeComponent },
+    { path: '', title: `Login | ${tituloPagina}`, component: LoginComponent },
+];
