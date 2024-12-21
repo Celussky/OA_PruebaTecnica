439b006947b7caf33b3ae22b9e422391137819be
Celussky <gabri.bernal@gmail.com>
2024-12-20 23:31:42 -0500

    footer terminado

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faFacebook = faFacebook
+  faTwitter = faTwitter
+  faGoogle = faGoogle
+  faInstagram = faInstagram
+  faLinkedin = faLinkedin
+  faGithub = faGithub
 }
